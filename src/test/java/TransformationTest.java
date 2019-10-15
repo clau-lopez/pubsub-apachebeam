d5bd99a421c7c394e8f59bc648ce9fb614a07ee5
Claudia Lopez <colopezfuentes@gmail.com>
2019-10-15 10:12:06 -0300

    Adding test

--- src/test/java/TransformationTest.java
+++ src/test/java/TransformationTest.java
@@ -1,26 +1,10 @@
-import org.apache.beam.repackaged.direct_java.runners.fnexecution.control.RemoteOutputReceiver;
 import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.DoFnOutputReceivers;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class TransformationTest {
 
-    @Test
-    void shouldBeEqualWhenLightIs20() {
-        Transformation transformation = new Transformation();
-
-        String information = "20,25.0,10,5,234:343:675,4,100000000";
-        SensorRecord sensorRecordExpected = new SensorRecord();
-        sensorRecordExpected.setSunlight(20);
-
-        DoFn.OutputReceiver outputReceiver = mock(DoFn.OutputReceiver.class);
-        transformation.processElement(information, outputReceiver);
-        verify(outputReceiver).output(sensorRecordExpected.toString());
-    }
     @Test
     void shouldCreateOutputWithTemperature() {
         Transformation transformation = new Transformation();
@@ -35,12 +19,23 @@ public class TransformationTest {
         sensorRecordExpected.setBattery(40);
         sensorRecordExpected.setTimestamp("100000000");
 
-
         DoFn.OutputReceiver outputReceiver = mock(DoFn.OutputReceiver.class);
         transformation.processElement(information, outputReceiver);
         verify(outputReceiver).output(sensorRecordExpected.toString());
     }
 
+    @Test
+    void shouldNotCreateOutput() {
+        Transformation transformation = new Transformation();
+
+        String information = "20,25.0,10,5,234:343:675,40";
+
+
+        DoFn.OutputReceiver outputReceiver = mock(DoFn.OutputReceiver.class);
+        transformation.processElement(information, outputReceiver);
+        verifyZeroInteractions(outputReceiver);
+
+    }
 
 
 }
